2c708f42dbcc673de8d6e5857ab3390d7ac4244d
becher <129062128+nbecherman@users.noreply.github.com>
2024-05-23 02:14:50 +0000

    separacion entre componentes y contenido

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import './App.css';
-import Header from './Components/Header';
-import Acerca from './Components/Acerca';
-import Conocimientos from './Components/conocimientos';
-import Experiencia from './Components/experiencia';
-import Educacion from './Components/educacion';
-import Contacto from './Components/contacto';
-import Olas from './Components/Olas';
-
-
-
+import "./App.css";
+import Header from "./Components/Header";
+import Acerca from "./Components/Acerca";
+import Conocimientos from "./Components/conocimientos";
+import Experiencia from "./Components/experiencia";
+import Educacion from "./Components/educacion";
+import Contacto from "./Components/contacto";
+import Olas from "./Components/Olas";
 
 function App() {
   return (
     <>
-    <Header/>
-    <Acerca/>
-    <Olas color="#0099ff" fondo="blanco"/>
-    <Conocimientos/>
-    <Olas color="#fff" fondo="azul"/>
-    <Experiencia/>
-    <Olas color="#0099ff" fondo="blanco"/>
-    <Educacion/>
-    <Olas color="#fff" fondo="azul"/>
-    <Contacto/>
-    <Olas color="#fff" fondo="azul"/>
-
-
-
-
-
+      <Header />
+      <Acerca />
+      <Olas color="#0099ff" fondo="blanco" />
+      <Conocimientos />
+      <Olas color="#fff" fondo="azul" />
+      <Experiencia />
+      <Olas color="#0099ff" fondo="blanco" />
+      <Educacion />
+      <Olas color="#fff" fondo="azul" />
+      <Contacto />
+      <Olas color="#0099ff" fondo="blanco" />
     </>
-    
   );
 }
 
